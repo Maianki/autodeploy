bba8296a2ef313a41ad88df6b392f0b366e0a914
Ankit Kumain <kumankit10@gmail.com>
2021-08-31 18:32:50 +0530

    added secret key variable

--- autodeploy/settings.py
+++ autodeploy/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'afzc@l0amhd@2#5ua34od4$&!6_y7d&=8)2_2ad+0x!en_=bdg'
+SECRET_KEY = SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["djangoautodeploy.herokuapp.com","127.0.0.1"]
 
